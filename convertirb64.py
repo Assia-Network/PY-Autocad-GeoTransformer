7c2042c5f101854140302bb592f8e94dad2a7b18
Assia-Network <jesusmartinofficial@gmail.com>
2026-02-26 20:09:17 -0500

    first commit

--- convertirb64.py
+++
@@ -1,5 +0,0 @@
-import base64
-with open("ico.png", "rb") as image_file:
-    texto_puro = base64.b64encode(image_file.read()).decode('utf-8')
-with open("b64_ico.txt", "w") as archivo:
-    archivo.write(texto_puro)
